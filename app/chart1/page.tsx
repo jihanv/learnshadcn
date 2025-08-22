dc40c9536c3d9276f14b3a637c30d9f0f5f5a552
jihanv <hedjmny@gmail.com>
2025-08-22 12:01:12 +0900

    Add chart

---
+++ app/chart1/page.tsx
@@ -0,0 +1,9 @@
+import { Chart1 } from "@/components/ui/example-chart";
+
+export default function page() {
+    return (
+        <div>
+            <Chart1 />
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <>
+        <div className="flex flex-col gap-9 m-9">
             <Link href="/card">
                 <Button className="hover:cursor-pointer" >Checkout Card Component</Button>
             </Link>
-        </>
+            <Link href="/chart1">
+                <Button className="hover:cursor-pointer" >Checkout chart</Button>
+            </Link>
+        </div>
     );
 }
